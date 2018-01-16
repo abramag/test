103ad523daf9613f178778a58728b18cd9d1066a
abramag <abramag@localhost>
2018-01-16 12:41:54 +0100

    not working

--- Konwer/src/LStart.java
+++ Konwer/src/LStart.java
@@ -3,7 +3,19 @@ public class LStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test");
+		System.out.println("test");
+	int k;
+	LStart.LStop stp = new LStart.LStop();
+	k=stp.LStopa(8);
+	System.out.println(k);
 	}
-
+	
+	
+	private class LStop {
+		 public int LStopa(int l) {
+			 l++;
+			 return l;
+		 }	
+			
+		}
 }
